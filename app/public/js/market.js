73709937c34b099125fa04b6a36ab703916c4d87
Pat Gronowski <patgronowski@users.noreply.github.com>
2017-12-03 17:13:36 -0500

    Add market buttons

--- app/public/js/market.js
+++ app/public/js/market.js
@@ -1,5 +1,5 @@
-/* Generate the market table using a call to our API. */
-document.getElementById("table").style.height = ((window.innerHeight - 100- document.getElementById("tableSearch").offsetHeight)) + "px"
+document.getElementById("table").style.height =
+	((window.innerHeight - 100 - document.getElementById("tableSearch").offsetHeight)) + "px";
 
 $("#tableSearch").keyup(function myFunction() {
 	// Declare variables
@@ -25,35 +25,35 @@ $("#tableSearch").keyup(function myFunction() {
 	}
 });
 
-// $(function() {
-// 	$.ajax({
-//     	type: 'GET',
-//     	url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10', // limit to 10 for now
-//     	success: function(data) {
-//     		$.each(data, function(index, item) {
-//
-// 				let tableRow = $("<tr>")
-// 				let rank = $("<td>").append(item.rank)
-//
-// 				let nameDiv = $("<div>").css({ "float": "left" }).append(item.name)
-// 				let button = newBuyButton(item.id).click(buyClickListener)
-//
-// 				let buttonDiv = $("<div>").css({ "float": "right" }).append(button)
-//
-// 				let nameAndButton = $("<td>").append(nameDiv).append(buttonDiv)
-//
-// 				let symbol = $("<td>").append(item.symbol)
-// 				let price = $("<td>").append(item.price_usd)
-// 				let marketCap = $("<td>").append(item.market_cap_usd)
-//
-// 				tableRow.append(rank).append(nameAndButton)
-// 						.append(symbol).append(price)
-// 						.append(marketCap).appendTo(".table-body")
-//     		})
-//
-//     	},
-//    	 	error: function(xhr, error) {
-//         	console.log("Something went wrong: ", error)
-//     	}
-// 	})
-// })
+$(function() {
+	$.ajax({
+    	type: 'GET',
+    	url: 'http://localhost:3000/api/coin-data', // limit to 10 for now
+    	success: function(data) {
+    		$.each(data, function(index, item) {
+
+				let tableRow = $("<tr>")
+				let rank = $("<td>").append(item.rank)
+
+				let nameDiv = $("<div>").css({ "float": "left" }).append(item.name)
+				let button = newBuyButton(item.id).click(buyClickListener)
+
+				let buttonDiv = $("<div>").css({ "float": "right" }).append(button)
+
+				let nameAndButton = $("<td>").append(nameDiv).append(buttonDiv)
+
+				let symbol = $("<td>").append(item.symbol)
+				let price = $("<td>").append(item.price_usd)
+				let marketCap = $("<td>").append(item.market_cap_usd)
+
+				tableRow.append(rank).append(nameAndButton)
+						.append(symbol).append(price)
+						.append(marketCap).appendTo(".table-body")
+    		})
+
+    	},
+   	 	error: function(xhr, error) {
+        	console.log("Something went wrong: ", error)
+    	}
+	})
+})
